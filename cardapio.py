40cbbeebca8df19ebfbf4b5dcc4ec2954ec8a3f5
Samuel L P Duque <samuellocatel@gmail.com>
2023-11-22 20:09:06 -0300

    Upd.

--- cardapio.py
+++ cardapio.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import ast
 estoque_df = pd.read_csv('estoque.csv')
 estoque = estoque_df.set_index('Item')['Quantidade'].to_dict()
 
@@ -16,10 +17,26 @@ cardapio = {"Lasanha":{"Trigo":2,"Tomate":1},
             "Risoto":{"Arroz":1,"Tomate":1,"Queijo":1},
             "Purê":{"Batata":1,"Creme de Leite":1,"Queijo":1},
             "Fetuccine":{"Macarrão":1,"Creme de Leite":1,"Queijo":1},
-            "Nhoque":{"Batata":1,"Trigo":1,"Queijo":1}}
+            "Nhoque":{"Batata":1,"Trigo":1,"Queijo":1},
+            "Bife":{"Carne":1,"Cebola":1,"Tomate":1},
+            "Omelete":{"Ovo":2,"Queijo":1},
+            "Filet ao Molho Madeira":{"Carne":1,"Creme de Leite":1,"Cebola":1,"Tomate":1},
+            "Filet au Poivre":{"Carne":1,"Creme de Leite":1,"Cebola":1,"Tomate":1},
+            "Peixe":{"Peixe":1,"Cebola":1,"Tomate":1},
+            "Frango grelhado":{"Frango":1,"Cebola":1,"Tomate":1},
+            "Frango ao Molho Branco":{"Frango":1,"Creme de Leite":1,"Cebola":1,"Tomate":1},
+            "Suco de Laranja":{"Laranja":2},"Suco de Limão":{"Limão":2},"Suco de Abacaxi":{"Abacaxi":2},
+            "Suco de Morango":{"Morango":2},"Suco de Uva":{"Uva":2},"Agua":{"Agua":2},"Coca-Cola":{"Coca-Cola":2},
+            "Guaraná":{"Guaraná":2},"Fanta":{"Fanta":2},"Sprite":{"Sprite":2}}
 
 
+cardapio_df = pd.read_csv('cardapio.csv')
 
+# Converte a coluna'Ingredientes' de string p/ dicionario
+cardapio_df['Ingredientes'] = cardapio_df['Ingredientes'].apply(ast.literal_eval)
+
+# Cria um dicionario com o nome da comida como chave e os ingredientes como valor
+cardapio = cardapio_df.set_index('Comida')['Ingredientes'].to_dict()
 
 i = 0
 while (i<2):
@@ -29,10 +46,6 @@ while (i<2):
             estoque[item] -= cardapio[comida][item]
     i=i+1
 
-print(estoque)
 
-cardapio_df = pd.DataFrame(list(cardapio.items()), columns=['Comida', 'Ingredientes'])
 estoque_df = pd.DataFrame(list(estoque.items()), columns=['Item', 'Quantidade'])
-
-cardapio_df.to_csv('cardapio.csv', index=False)
 estoque_df.to_csv('estoque.csv', index=False)
