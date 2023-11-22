24600f8feb1b204d89e91e718a89b117ce8afc35
Samuel L P Duque <131277691+Samuel-Duque@users.noreply.github.com>
2023-11-22 11:48:03 -0300

    Update tela_menu_login-cadastro.py

--- tela_menu_login-cadastro.py
+++ tela_menu_login-cadastro.py
@@ -1,16 +1,33 @@
 class Cliente():
-    def __init__(self, nome, password):
+    def __init__(self, nome, password,cargo):
         self.nome = nome
         self.password = password
-
-def Cadastro(lista_cadastrados):
+        self.cargo = cargo
+def Cadastro_Gerente(lista_cadastrados,cargos):
+    
     nome_cadastro = input("Digite o seu nome para cadastro: ")
     senha_cadastro = input("Digite sua senha para cadastro: ")
     senha_cadastro_verificacao = input("Digite sua senha novamente para cadastro: ")
     if senha_cadastro != senha_cadastro_verificacao:
         print("Senhas diferentes!")
-        Cadastro(lista_cadastrados)
-    cadastro_novo = Cliente(nome_cadastro, senha_cadastro)
+        Cadastro_Gerente(lista_cadastrados,cargos)
+    cadastro_novo = Cliente(nome_cadastro, senha_cadastro,cargos[0])
+    lista_cadastrados.append(cadastro_novo)
+
+def Cadastro_Funcionarios(lista_cadastrados, cargos):
+    
+    nome_cadastro = input("Digite o seu nome para cadastro: ")
+    senha_cadastro = input("Digite sua senha para cadastro: ")
+    senha_cadastro_verificacao = input("Digite sua senha novamente para cadastro: ")
+    if senha_cadastro != senha_cadastro_verificacao:
+        print("Senhas diferentes!")
+        Cadastro_Gerente(lista_cadastrados,cargos)
+
+    for index,cargo in enumerate(cargos):
+        print(f"{index} - {cargo}")
+
+    escolher_cargo= int(input("Digite o index do cargo desejado: "))
+    cadastro_novo = Cliente(nome_cadastro, senha_cadastro,cargos[escolher_cargo])
     lista_cadastrados.append(cadastro_novo)
 
 def Login(lista_cadastrados):
@@ -18,25 +35,51 @@ def Login(lista_cadastrados):
     entrada_senha = input("Digite sua senha: ")
     for pessoa in lista_cadastrados:
         if pessoa.nome == entrada_nome and pessoa.password == entrada_senha:
-            print("LOGADO COM SUCESSO")
+            print("\nLOGADO COM SUCESSO")
+            print(f"Nome: {pessoa.nome},Cargo:{pessoa.cargo}")
         elif pessoa.nome == entrada_nome and pessoa.password != entrada_senha:
-            print("SENHA INCORRETA")
+            print("\nSENHA INCORRETA")
+        else:
+            print("\nUsuario não encontrado!")
 
-def main():
-    lista_cadastrados = []
+def main_gerente(lista_cadastrados,cargos):
     while True:
         print('\n')
         print("-*"*30)
         print(' '*23,"TELA DE LOGIN")
         print("-*"*30)
-        print("1. Cadastrar funcionario")
-        print("2. LOGIN")
-
+        print("1. Cadastrar Funcionario")
+        print("2. Registro")
+        print("2. Relatorio")
+        print("3. SAIR")
         entrada = input("Digite a opcao desejada: ")
         match entrada:
             case '1':
-                Cadastro(lista_cadastrados)
+                Cadastro(lista_cadastrados,cargos)
             case '2':
                 Login(lista_cadastrados)
+            case '3':
+                break
+
+            
+def main():
+    lista_cadastrados = []
+    cargos=['Gerente','Chef de cozinha','Cliente']
+    while True:
+        print('\n')
+        print("-*"*30)
+        print(' '*23,"TELA DE LOGIN")
+        print("-*"*30)
+        print("1. Cadastrar Gerente")
+        print("2. LOGIN")
+        print("3. SAIR")
+        entrada = input("Digite a opcao desejada: ")
+        match entrada:
+            case '1':
+                Cadastro_Gerente(lista_cadastrados,cargos)
+            case '2':
+                Login(lista_cadastrados)
+            case '3':
+                break
 if __name__ == '__main__':
     main()
